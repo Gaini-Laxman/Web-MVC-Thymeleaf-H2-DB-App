8dfe2648eee61a47b7b6fcd3458ae30adae20713
Gaini Laxman <gainilaxman12@gmail.com>
2023-04-11 20:15:57 +0530

    added form validation and edit option

--- src/main/java/com/klinnovations/controller/ProductController.java
+++ src/main/java/com/klinnovations/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.klinnovations.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,9 +23,21 @@ public class ProductController {
 	@Autowired
 	private ProductRepository repo;
 	
+	
+	@GetMapping("/edit")
+	public String edit(@RequestParam("pid") Integer pid, Model model) {
+		
+	  Optional<Product> findById = repo.findById(pid);
+	  if(findById.isPresent()) {
+		  Product product = findById.get();
+		  model.addAttribute("product", product);
+	  }
+	  return "index";
+	  
+	}
+	
 	@GetMapping("/delete")
 	public String delete(@RequestParam("pid") Integer pid, Model model) {
-		
 		
 		repo.deleteById(pid);
 		model.addAttribute("msg", "Product Deleted...");
@@ -34,7 +49,14 @@ public class ProductController {
 
 	//Saving Product Detail
 	@PostMapping("/product")
-	public String saveProduct(@ModelAttribute("product") Product p, Model model) {
+	public String saveProduct(@Validated @ModelAttribute("product") Product p, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) {
+          return"index";			
+			
+		}
+		
+		
 		p = repo.save(p);
 		if (p.getPid() != null) {
 			model.addAttribute("msg", "Product Saved...!");
--- src/main/java/com/klinnovations/entity/Product.java
+++ src/main/java/com/klinnovations/entity/Product.java
@@ -3,6 +3,11 @@ package com.klinnovations.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Entity
@@ -12,8 +17,17 @@ public class Product {
 	@Id
 	@GeneratedValue
 	private Integer pid;
+	
+	@Size(min=3, max=15, message="size should be between 3 to 15")
+	@NotBlank(message="Name is Mandatory")
 	private String name;
+	
+	@NotNull(message="Price is Mandatory")
+	@Positive(message="Price Should be Positive")
 	private Double price;
+	
+	@NotNull(message="Quantity is Mandatory")
+	@Positive(message="Quantity should be Positive")
 	private Integer quantity;
 
 }
